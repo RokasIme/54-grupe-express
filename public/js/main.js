cce8cd358f0ffc99429711fbb825701963689e8c
Rokas Imelinskas <rokas.imelinskas@gmail.com>
2025-05-06 14:59:59 +0300

    dumenys siunčiami iš lauko

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,32 @@
 console.log("Client JS logic....");
 
-const formDOM = document.querySelector("form");
+// const formDOM = document.querySelector("form");
+const formDOM = document.forms[0];
+
 if (formDOM) {
   const textareaDOM = formDOM.querySelector("textarea");
   const btnDOM = formDOM.querySelector("button");
+
+  formDOM.addEventListener("submit", async (event) => {
+    event.preventDefault(); //neleidžia naršyklei nueiti į /text. palieka kaip yra
+
+    const data = {
+      text: textareaDOM.value,
+    };
+    // const jsonData = JSON.stringify(data); //paverčia į stringą
+
+    const result = await fetch("/api/text", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json", //''Content-Type" reikia būtinai rašyti su kabutėmis nes du žodžiai ir minusas - būtų kaip matematinis operatorius
+      },
+      body: JSON.stringify(data),
+    }); //fetch siunčia užklausą, kreipiasi į adresą kaip GET, POST
+    const resultData = await result.json();
+
+    // .then((data) => data.json()) //duomenis pavečiame į JSON
+    //   .then((data) => console.log(data));
+
+    console.log(resultData);
+  });
 }
--- src/api/text/textPost.js
+++ src/api/text/textPost.js
@@ -1,6 +1,8 @@
 import { textList } from "../../pages/home.js";
 
 export function textPost(req, res) {
+  console.log("---");
+  console.log(req.body);
   if (typeof req.body === "object" && typeof req.body.text === "string") {
     textList.push(req.body.text);
     return res.json({
